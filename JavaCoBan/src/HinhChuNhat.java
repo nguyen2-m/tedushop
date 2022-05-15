6252845c23639c89c44d404a496e4c82400f0c59
Administrator <Administrator@MAYTINHCUAHOA>
2022-05-15 17:35:12 +0900

    vidu1

---
+++ JavaCoBan/src/HinhChuNhat.java
@@ -0,0 +1,13 @@
+
+public class HinhChuNhat {
+	
+	public static float chuVi(float chieuDai, float chieuRong) {
+		return (chieuDai + chieuRong) *2;
+		
+	}
+	public static float dienTich(float chieuDai, float chieuRong) {
+		return chieuDai * chieuRong;
+		
+	}
+
+}
